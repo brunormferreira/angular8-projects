e0bcea6850ae988386a54ec49ff44f8fa45d925c
brunormferreira <brunormferreira@hotmail.com>
2019-10-18 15:22:38 -0300

    add: TWO way d-binding
    using form TD

--- training-angular/src/app/home/home.component.ts
+++ training-angular/src/app/home/home.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   
   clickCounter: number = 0;
-
+  name: string = '';
+  
   constructor() { }
 
   ngOnInit() {
