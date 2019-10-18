24b61361aee6e636b810ce2deeaba21164416257
brunormferreira <brunormferreira@hotmail.com>
2019-10-18 15:48:39 -0300

    add: styling stuffs
    like a ngStyle/ [style.property] / [class.active]/ ..

--- training-angular/src/app/home/home.component.ts
+++ training-angular/src/app/home/home.component.ts
@@ -18,4 +18,12 @@ export class HomeComponent implements OnInit {
   countClick() {
     this.clickCounter += 1;
   }
+
+  setClasses() {
+    let myClasses = {
+      active: this.clickCounter > 4,
+      notactive: this.clickCounter <= 4
+    }
+    return myClasses;
+  }
 }
