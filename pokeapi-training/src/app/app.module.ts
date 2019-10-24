b46183d4d7663724b08c659d550105739cd4564c
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-24 11:17:10 -0300

    add: routes and more components

--- pokeapi-training/src/app/app.module.ts
+++ pokeapi-training/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { PokeListComponent } from './components/poke-list/poke-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    PokeListComponent
   ],
   imports: [
     BrowserModule,
