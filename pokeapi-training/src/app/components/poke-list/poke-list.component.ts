e6dfeea2864cef120178953bf380a98454f881cb
Bruno Ramires <brunormferreira@hotmail.com>
2019-10-24 15:37:39 -0300

    add: create components and make services ok

--- pokeapi-training/src/app/components/poke-list/poke-list.component.ts
+++ pokeapi-training/src/app/components/poke-list/poke-list.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
+import { PokeAPI, Results, PokemonDetails } from 'src/interfaces';
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent implements OnInit {
+export class PokeListComponent implements OnInit, OnDestroy {
+  @Output() exportPokemons = new EventEmitter();
+  pokemons: PokeAPI;
+  pokemonsSub: Subscription;
+  pokemonsLoaded: boolean;
+  // query: string;
+  // abilityFilters: Array<string>;
+  // typeFilters: string;
 
-  constructor() { }
+  constructor(private pokemonService: PokemonService) { }
 
   ngOnInit() {
+    this.getPokemons();
   }
 
+  getPokemons(): void {
+    this.pokemonsSub = this.pokemonService.getPokemon().subscribe(
+      (data: PokeAPI) => {
+        this.pokemons = data;
+        console.log(this.pokemons.results);
+        if (this.pokemons.results && this.pokemons.results.length) {
+          // get pokemon details for every pokemon
+          this.pokemons.results.forEach(pokemon => {
+            // set pokemon id
+            pokemon.id = pokemon.url.split('/')[
+              pokemon.url.split('/').length - 2
+            ];
+
+            this.getPokemonDetails(pokemon);
+            this.getPokemonSpeciesDetails(pokemon);
+          });
+        }
+      }
+    );
+  }
+
+  getPokemonDetails(pokemon: Results): void {
+    this.pokemonService
+      .getPokemonDetails(pokemon.name)
+      .subscribe((details: PokemonDetails) => {
+        pokemon.details = details;
+        // when last pokemon details have been loaded
+        // send pokemons to header component
+        if (pokemon.id === '151') {
+          this.pokemonsLoaded = true;
+          this.exportPokemons.emit(this.pokemons.results);
+        }
+      });
+  }
+
+  getPokemonSpeciesDetails(pokemon: Results): void {
+    this.pokemonService
+      .getPokemonSpecies(pokemon.name)
+      .subscribe((species: any) => {
+        const entries = species.flavor_text_entries;
+        if (entries) {
+          entries.some(flavor => {
+            if (flavor.language.name === 'en') {
+              pokemon.description = flavor.flavor_text;
+            }
+          });
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.pokemonsSub.unsubscribe();
+  }
 }
---
+++ pokeapi-training/src/app/services/pokemon.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { PokeAPI, PokemonDetails } from 'src/interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+  pokeAPI: any;
+  pokeSpeciesAPI: any;
+
+  constructor(private http: HttpClient) {
+    this.pokeAPI = environment.pokemonURL;
+    this.pokeSpeciesAPI = environment.pokemonSpeciesURL;
+  }
+
+  getPokemon(): Observable<PokeAPI> {
+    return this.http
+      .get<PokeAPI>(`${this.pokeAPI}?limit=151`)
+      .pipe(catchError(this._handleError));
+  }
+
+  getPokemonDetails(name): Observable<PokemonDetails> {
+    return this.http
+      .get<PokemonDetails>(`${this.pokeAPI}/${name}`)
+      .pipe(catchError(this._handleError));
+  }
+
+  /**
+   * Uses pokemon name to retrieve individual pokemon species details
+   */
+  getPokemonSpecies(name): Observable<any> {
+    return this.http
+      .get<any>(`${this.pokeSpeciesAPI}/${name}`)
+      .pipe(catchError(this._handleError));
+  }
+
+  private _handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened. Please try again later.');
+  }
+}
