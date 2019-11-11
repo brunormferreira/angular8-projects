5cc28617d7378d8178f41774033c08103aee7e65
Bruno Ramires <brunormferreira@hotmail.com>
2019-11-11 13:45:22 -0300

    add: using @Input props to pass data
    Father (painel) to children (progresso)

--- learning-english/src/app/progresso/progresso.component.ts
+++ learning-english/src/app/progresso/progresso.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-progresso',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressoComponent implements OnInit {
 
-  progress: number = 25;
+  @Input() progress: number = 0;
 
   constructor() { }
 
