323d40bed6059871c494b6632ff06a65e22452cb
Bruno Ramires <brunormferreira@hotmail.com>
2019-11-12 13:41:34 -0300

    add: oferta service and model
    call data from service to a home component

--- passaro-urbano/src/app/home/home.component.ts
+++ passaro-urbano/src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ OfertasService ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  ofertas: Oferta[];
+
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.ofertasService.getOfertas();
+    console.log(this.ofertas);
   }
 
 }
