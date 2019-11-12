8e9e97158bf04eddbedd533f872c4508218cf9c4
Bruno Ramires <brunormferreira@hotmail.com>
2019-11-12 13:49:09 -0300

    add: showing service-base on offers

--- passaro-urbano/src/app/home/home.component.ts
+++ passaro-urbano/src/app/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.ofertas = this.ofertasService.getOfertas();
-    console.log(this.ofertas);
+    // console.log(this.ofertas);
   }
 
 }
