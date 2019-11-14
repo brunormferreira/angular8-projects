346f081a2c7c526763e54e6b4e3bccaba2aac3a7
Bruno Ramires <brunormferreira@hotmail.com>
2019-11-14 15:05:14 -0300

    add: retrieving selected offer by ID and create his method on service

--- passaro-urbano/src/app/oferta/oferta.component.ts
+++ passaro-urbano/src/app/oferta/oferta.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [ OfertasService ]
 })
 export class OfertaComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  oferta: Oferta;
+
+  constructor(private route: ActivatedRoute,
+              private ofertasService: OfertasService) { }
 
   ngOnInit() {
-    // console.log(this.route.snapshot.params['id']);
-    this.route.params.subscribe((parametro: any) => {
-      console.log(parametro.id);
-    });
+    // this.route.params.subscribe((parametro: any) => { console.log(parametro.id); });
+    this.ofertasService.getOfertaPorId(this.route.snapshot.params.id)
+      .then((oferta: Oferta) => {
+        // console.log(oferta);
+        this.oferta = oferta;
+      });
   }
 
 }
--- passaro-urbano/src/app/ofertas.service.ts
+++ passaro-urbano/src/app/ofertas.service.ts
@@ -20,4 +20,13 @@ export class OfertasService {
       .toPromise()
       .then((resposta: any) => resposta);
   }
+
+  public getOfertaPorId(id: number): Promise<Oferta> {
+    return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+      .toPromise()
+      .then((resposta: any) => {
+        // console.log(resposta.shift());
+        return resposta[0];
+      });
+  }
 }
