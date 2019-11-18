0c834635d8f030f5b586ce55dfea58b9ee4df555
Bruno Ramires <brunormferreira@hotmail.com>
2019-11-18 09:57:31 -0300

    add: using environment variables

--- passaro-urbano/src/app/ofertas.service.ts
+++ passaro-urbano/src/app/ofertas.service.ts
@@ -2,27 +2,27 @@ import { Oferta } from './shared/oferta.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from '../environments/environment';
+
 @Injectable()
 export class OfertasService {
 
   constructor(private http: HttpClient) {}
 
-
-
   public getOfertas(): Promise<Oferta[]> {
-    return this.http.get('http://localhost:3000/ofertas?destaque=true')
+    return this.http.get(`${environment.URL_API}?destaque=true`)
       .toPromise()
       .then((resposta: any) => resposta);
   }
 
   public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-    return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+    return this.http.get(`${environment.URL_API}?categoria=${categoria}`)
       .toPromise()
       .then((resposta: any) => resposta);
   }
 
   public getOfertaPorId(id: number): Promise<Oferta> {
-    return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+    return this.http.get(`${environment.URL_API}?id=${id}`)
       .toPromise()
       .then((resposta: any) => {
         // console.log(resposta.shift());
